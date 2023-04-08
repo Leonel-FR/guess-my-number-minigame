9f68ba1e6698a91ed508f3718eddc050fb510d8a
Leonel Ruiz <114844771+Leonel-FR@users.noreply.github.com>
2023-04-07 22:36:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const Secretnumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === Secretnumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('.number').textContent = Secretnumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('number').style.width = '30rem';
+
+    //FIXME: ------------------------
+    if (score > document.querySelector('.highscore').textContent) {
+      document.querySelector('.highscore').textContent = score;
+    } else {
+      score = score;
+    }
+    //-----------------------------------
+  } else if (guess > Secretnumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < Secretnumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
+});
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
+  // It will just simply reload the page and will set everything to their initial valu
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('number').style.width = '15rem';
+});
